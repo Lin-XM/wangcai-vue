3e2a89eafb8d492afc18c6da6b19184abf6c144d
xiaoming <2604863701@qq.com>
2022-06-12 10:15:39 +0800

    上半部分样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,8 +14,8 @@
             </div>
             <div>
                 <label class="notes">
-                    <span class="name">备注</span>
-                    <input type="text">
+                    <span class="name">备注:</span>
+                    <input type="text" placeholder="请输入您的备注">
                 </label>
             </div>
             <div>
@@ -51,9 +51,86 @@
 
 <script lang="ts">
   export default {
-    name: 'Money.vue',
+    name: 'Money',
   };
 </script>
 
 <style scoped lang="scss">
+    @import "~@/assets/styles/helper.scss";
+
+    .tags {
+        font-size: 14px;
+        padding: 16px;
+
+        > .current {
+            display: flex;
+
+            > li {
+                background-color: rgb(192, 217, 255);
+                height: 24px;
+                line-height: 24px;
+                border-radius: (24px/2);
+                padding: 0 16px;
+                margin-right: 12px;
+            }
+        }
+
+        > .new {
+            padding-top: 16px;
+
+            button {
+                background-color: transparent;
+                border: none;
+                color: #999;
+                border-bottom: 1px solid;
+                padding: 0 4px;
+            }
+        }
+    }
+
+    .notes {
+        background-color: #cee7e7;
+        font-size: 14px;
+        padding-left: 16px;
+        display: flex;
+        align-items: center;
+
+        .name {
+            padding-right: 16px;
+        }
+
+        input {
+            height: 64px;
+            flex-grow: 1;
+            background-color: transparent;
+            border: none;
+            padding-right: 16px;
+        }
+    }
+
+    .types {
+        background-color: lightseagreen;
+        display: flex;
+        text-align: center;
+        font-size: 24px;
+        > li {
+            width: 50%;
+            height: 64px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            vertical-align: top;
+            position: relative;
+            &.selected::after{
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0 ;
+                width: 100%;
+                height: 3px;
+                background-color: #2853d2;
+            }
+        }
+    }
+
 </style>
