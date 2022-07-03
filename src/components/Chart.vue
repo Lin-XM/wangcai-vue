5debe55ce308035a5febb45974a5a8aeb139c89d
xiaoming <2604863701@qq.com>
2022-07-03 10:36:25 +0800

    修复 bug

---
+++ src/components/Chart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="wrapper" ref="wrapper"></div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator';
+  import * as echarts from 'echarts';
+  import {EChartOption} from 'echarts';
+
+  @Component
+  export default class Chart extends Vue {
+    @Prop() options?: EChartOption;
+
+    mounted() {
+      console.log(this.options);
+      if (this.options === undefined) {
+        console.log('没有数据');
+        return;
+      }
+      let chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+      chart.setOption(this.options);
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .wrapper {
+        height: 400px;
+    }
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,7 @@
     <div>
         <Layout>
             <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+            <Chart :options="x" />
             <ol v-if="groupList.length>0">
                 <li v-for="(group,index) in groupList" :key="index">
                     <h3 class="title">{{beautify(group.title)}}<span>￥{{group.total}}</span></h3>
@@ -30,9 +31,11 @@
   import recordTypeList from '@/constants/recordTypeList.ts';
   import dayjs from 'dayjs';
   import clone from '@/lib/clone';
+  import Chart from "@/components/Chart.vue"
+
 
   @Component({
-    components: {Tabs},
+    components: {Tabs, Chart},
   })
   export default class Statistics extends Vue {
     tagString(tags: Tag[]) {                                                          // 处理标签
@@ -55,17 +58,46 @@
       }
     }
 
+
+    get x() {
+      return {
+        xAxis: {
+          type: 'category',
+          data: [
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+          ]
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+          ],
+          type: 'line'
+        }],
+        tooltip: {show: true}
+      };
+    }
+
     get recordList() {
       return (this.$store.state as RootState).recordList;
     }
 
     get groupList() {
       const {recordList} = this;
+
       type Result = {
         title: string,
         total?: number,
         items: RecordItem[]
       }[]
+
 
       // 对创建时间 进行排序
       const newList = clone(recordList)
@@ -73,10 +105,11 @@
         .sort((a, b) => dayjs(b.createAt)
           .valueOf() - dayjs(a.createAt).valueOf());
 
-      if (recordList.length === 0) { return []; }
+      if (newList.length === 0) { return []; }
 
 
       const result: Result = [{title: dayjs(newList[0].createAt).format('YYYY-MM-DD'), items: [newList[0]]}];      // 将newList 的第一项放进去
+
       for (let i = 1; i < newList.length; i++) {
         const currentItem = newList[i];
         const last = result[result.length - 1];
@@ -87,6 +120,8 @@
           result.push({title: dayjs(currentItem.createAt).format('YYYY-MM-DD'), items: [currentItem]});
         }
       }
+
+
       // 算出一天的支出/收入 总价格
       result.map(group => {
         group.total = group.items.reduce((sum, item) => {return sum + item.amount;}, 0);
@@ -107,6 +142,10 @@
 </script>
 
 <style scoped lang="scss">
+    .echarts {
+        max-width: 100%;
+        height: 400px;
+    }
     .noResult {
         padding: 16px;
         text-align: center;
